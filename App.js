985427296d63641db89aa99fe8a9ec5aefc18718
graemebrooks <graemebrooks@gmail.com>
2020-03-26 14:19:32 -0500

    adds header component

--- App.js
+++ App.js
@@ -1,19 +1,36 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import Header from './components/header';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+	const [ todos, setTodos ] = useState([
+		{ text: 'buy coffee', key: '1' },
+		{ text: 'create an app', key: '2' },
+		{ text: 'plot global domination', key: '3' }
+	]);
+
+	return (
+		<View style={styles.container}>
+			<Header />
+			<View style={styles.content}>
+				{/* todo form */}
+				<View style={styles.list}>
+					<FlatList data={todos} renderItem={({ item }) => <Text>{item.text}</Text>} />
+				</View>
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff'
+	},
+	content: {
+		padding: 40
+	},
+	list: {
+		marginTop: 20
+	}
 });
