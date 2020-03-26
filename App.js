95112e860743be047a51cac0d8ebab8f4bfc7bc2
graemebrooks <graemebrooks@gmail.com>
2020-03-26 18:57:14 -0500

    adds create todo functionality

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
 import Header from './components/header';
 import TodoItem from './components/todoItem';
+import AddTodo from './components/addTodo';
 
 export default function App() {
 	const [ todos, setTodos ] = useState([
@@ -14,14 +15,19 @@ export default function App() {
 		setTodos((prevTodos) => {
 			return prevTodos.filter((todo) => todo.key != key);
 		});
-		console.log(todos);
+	};
+
+	const submitHandler = (text) => {
+		setTodos((prevTodos) => {
+			return [ { text: text, key: Math.random().toString() }, ...prevTodos ];
+		});
 	};
 
 	return (
 		<View style={styles.container}>
 			<Header />
 			<View style={styles.content}>
-				{/* todo form */}
+				<AddTodo submitHandler={submitHandler} />
 				<View style={styles.list}>
 					<FlatList
 						data={todos}
